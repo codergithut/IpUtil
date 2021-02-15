6ccc61f0db80133c5b053f83061a921309e65342
田剑 <1468195034@qq.com>
2021-02-15 14:28:58 +0800

    Initial commit

---
+++ src/main/java/NetClientDemo.java
@@ -0,0 +1,48 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetClientOptions;
+import io.vertx.core.net.NetSocket;
+
+
+public class NetClientDemo extends AbstractVerticle {
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(new NetClientDemo());
+    }
+
+
+
+
+    @Override
+    public void start() throws Exception {
+
+
+        //创建连接到指定主机和端口的客户端，并绑定创建结果的处理器
+        NetClient netClient3 = vertx.createNetClient(new NetClientOptions().setConnectTimeout(10000))
+                .connect(9984, "localhost", res -> {
+                    if (res.succeeded()) {
+                        System.out.println("连接成功!");
+                        NetSocket socket = res.result();
+                        //向服务器写入数据
+                        socket.write(Buffer.buffer("发送数据......"), ar -> {
+                            if (ar.succeeded()) {
+                                System.out.println("数据发送成功!");
+                            } else {
+                                System.err.println("数据发送失败!");
+                            }
+                        });
+
+                        //读取服务端返回的数据
+                        socket.handler(buffer -> {
+                            System.out.println("读取到数据:" + buffer.toString() + " 长度为: " + buffer.length());
+                        });
+                        socket.closeHandler(ar -> {
+                            System.out.println("客户端断开连接");
+                        });
+                    } else {
+                        System.out.println("连接失败!: " + res.cause().getMessage());
+                    }
+                });
+    }
+}
---
+++ src/main/java/NetServerDemo.java
@@ -0,0 +1,45 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetServer;
+
+public class NetServerDemo extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        Vertx.vertx().deployVerticle(new NetServerDemo());
+    }
+
+
+
+
+    @Override
+    public void start() throws Exception {
+
+        //绑定处理器，当有请求进入时触发
+        NetServer netServer = vertx.createNetServer().connectHandler(netSocket -> {
+            System.out.println(netSocket.remoteAddress().host());
+            //得到NetSocket实例
+            netSocket.handler(buffer -> {
+                //读取数据
+                System.out.println("读取到数据:" + buffer.toString() + " 长度为: " + buffer.length());
+            });
+
+            netSocket.write(Buffer.buffer("数据已接收......"), ar -> {
+                if (ar.succeeded()) {
+                    System.out.println("写入数据成功!");
+                } else {
+                    System.err.println("写入数据失败!");
+                }
+            });
+            netSocket.closeHandler(ar -> {
+                System.out.println("客户端退出连接");
+            });
+        }).listen(9984, "localhost", res -> {
+            if (res.succeeded()) {
+                System.out.println("Tcp服务端启动成功");
+            } else {
+                System.err.println("Tcp服务端启动失败");
+            }
+        });
+    }
+}
